c2c2b453e244c64755e115a47efe87bd4399070c
Quorsor <96727527+Quorsor@users.noreply.github.com>
2023-01-04 18:39:20 +0800

    upload to site

---
+++ Javascript/Quiz.js
@@ -0,0 +1,9 @@
+
+
+function to_question(question) {
+    if (question == cur_que) {
+    }
+    else if (question != current) {
+        cur_que = question;
+    }
+}
---
+++ Javascript/questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, questions, options, and answers
+var questions = [
+    {
+        numb:1,
+        question: "Sino ro aton nga teacher sa Copro 4?",
+        answer: "Jorlyn Molas",
+        options: [
+            "Aila Shean Masinda Luna",
+            "James Masinda",
+            "Jorlyn Molas",
+            "Joana Mosquera",
+        ]
+    },
+    {
+        numb:2,
+        question: "Sino ro aton nga teacher sa Filipino?",
+        answer: "Joana Mosquera",
+        options: [
+            "James Masinda",
+            "Joana Mosquera",
+            "Jorlyn Molas",
+            "Aila Shean Masinda Luna",
+        ]
+    },
+    {
+        numb:3,
+        question: "Sino ro aton nga teacher sa Copro 5?",
+        answer: "Jorlyn Molas",
+        options: [
+            "Aila Shean Masinda Luna",
+            "James Masinda",
+            "Jorlyn Molas",
+            "Joana Mosquera",
+        ]
+    },
+    {
+        numb:4,
+        question: "Sino ro aton nga teacher sa English for Acads",
+        answer: "Aila Shean Masinda Luna",
+        options: [
+            "Jorlyn Molas",
+            "James Masinda",
+            "Aila Shean Masinda Luna",
+            "Joana Mosquera",
+        ]
+    },
+    {
+        numb:5,
+        question: "Sino ro aton nga teacher sa P.E?",
+        answer: "James Masinda",
+        options: [
+            "Aila Shean Masinda Luna",
+            "James Masinda",
+            "Jorlyn Molas",
+            "Joana Mosquera",
+        ]
+    }
+];
